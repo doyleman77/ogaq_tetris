e6fd34cb724b8f992d4842088146a631782f27ee
Nathan Miller <nathanmiller77@gmail.com>
2017-06-12 22:38:04 -0500

    update

--- game.cpp
+++ game.cpp
@@ -7,12 +7,15 @@ Game::Game()
     {
         std::cout << "Error initializing SDL!" << std::endl;
     }
-    window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    screen_width = 420; /// 320 for tetronimos' 10w grid and 100 for extra UI
+    screen_height = 740; /// 640 for tetronimos' 20h grid and 100 for extra UI
+    window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN);
 };
 
 Game::Game(int width, int height)
 {
-
+    screen_width = width;
+    screen_height = height;
 };
 
 
--- game.hpp
+++ game.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "SDL2/SDL.h"
+#include <iostream>
+#include <map>
+#include <string>
 
 class Game
 {
@@ -8,15 +11,19 @@ public:
     Game(int width, int height);
     ~Game();
 
-    play();
-    draw();
-    update();
+    void    play();
+    void    draw();
+    void    update();
 
 private:
+    int score;
+    short fps, level;
+    int[20][10] grid;
     bool running, paused;
     int screen_width, screen_height;
     SDL_Window* window;
     SDL_Renderer* renderer;
-    int score;
-    short fps, level;
+
+    std::map<std::string, SDL_Texture*> texture_cache;
+
 };
